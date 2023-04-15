62b81ebe3453b24a1f6f5e6b4ee1a59119856156
downinahill <brian.hill.0023@gmail.com>
2023-04-15 00:41:08 -0400

    initial commit

---
+++ src/components/Employees.js
@@ -0,0 +1,22 @@
+const Employees = [
+    {
+        id: "1",
+        Name: "Brian Hill",
+        Age: "37",
+
+    },
+    {
+        id: "2",
+        Name: "Bruce Banner",
+        Age: "46",
+        
+    },
+    {
+        id: "3",
+        Name: "Brandon Hall",
+        Age: "51",
+        
+    }
+]
+
+export default Employees
